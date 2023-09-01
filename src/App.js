2db1dce4484499b4624921c9bf5967eaad292abd
Jerrwyz <jerroldwongyz@gmail.com>
2023-09-01 17:15:17 +1000

    [JW] cleaned up code structure

--- src/App.js
+++ src/App.js
@@ -1,85 +1,12 @@
 import React from 'react';
-import NavBarComponent from './component/NavBarComponent';
-// import { Routes, Route, Link } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import { experimentalStyled as styled } from '@mui/material/styles';
-import FooterComponent from './component/FooterComponent';
-import CarouselComponent from './component/CarouselComponent';
-import ImageListComponent from './component/ImageListComponent';
-
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+import HomePage from './pages/HomePage';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
 
-        <NavBarComponent/>
+        <HomePage></HomePage>
 
-        <CarouselComponent/>
-
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6} md={3}>
-            <Item>
-              <ImageListComponent/>
-            </Item>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <Item>
-              <ImageListComponent/>
-            </Item>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <Item>
-              <ImageListComponent/>
-            </Item>
-          </Grid>
-          
-          <Grid item xs={12} sm={6} md={3}>
-            <Item>
-              <ImageListComponent/>
-            </Item>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <Item>
-              <ImageListComponent/>
-            </Item>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <Item>
-              <ImageListComponent/>
-            </Item>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <Item>
-              <ImageListComponent/>
-            </Item>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <Item>
-              <ImageListComponent/>
-            </Item>
-          </Grid>  
-        </Grid>
-        
-        <FooterComponent/>
-
-      </div>
     );
   }
 }
